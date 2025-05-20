9b6b2fb9b2760861d52b728781dabf97b4571984
Laxmi Kumari <laxmi87892@gmail.com>
2025-05-20 16:42:59 +0530

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -2,15 +2,10 @@
 .SYNOPSIS
     Check Bitlocker Compliance - Remediation Script
     OS Support: Windows 8 and above
-    Powershell: 4.0 and above
-    Run Type: Evaluation or OnDemand
 .DESCRIPTION
     This Worklet is designed to grant an Admin the ability to check the Bitlocker compliance of a device that falls
     within the defined range of System Types during the evaluation script. If this Worklet was ran manually, the system
     type check will be ignored and all and devices will report drive status to the activity log.
-.NOTES
-    Author: Tony Wiese
-    Date: March 19, 2021
 #>
 
 # Count Drives and initialize lists for later output
